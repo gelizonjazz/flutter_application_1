631e07993551988c9354eaf7eab7c4761f7ae03d
gelizonjazz <gelizonkarljustine@gmail.com>
2025-08-15 10:28:37 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,15 @@
+import 'Gelizon.dart';
+import 'Paa.dart';
+import 'Daga.dart';
+import 'Delis.dart';
 
+viod main() {
+  var person = Person('Gelizon', 18);
+  var person = Person('Paa', 19);
+  var person = Person('Daga', 18);
+  var person = Person('Delis', 18);
+
+  person1.sayHello(); // Output: Hello! I am Gelizon and I am 18 years old.
+  person2.sayHello(); // Output: Hello! I am Paa and I am 19 years old. 
+  person3.sayHello(); // Output: Hello! I am Daga and I am 18 years old. 
+  person4.sayHello(); // Output: Hello! I am Delis and I am 18 years old. 
