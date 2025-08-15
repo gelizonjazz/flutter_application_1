c759249ee31b05ac49b4f964eb96a980b535828b
gelizonjazz <gelizonkarljustine@gmail.com>
2025-08-15 11:20:55 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
-import 'Gelizon.dart';
-import 'Paa.dart';
-import 'Daga.dart';
-import 'Delis.dart';
+import 'student.dart';
+import 'ceo.dart';
+import 'manager.dart';
+import 'engineer.dart';
 
-viod main() {
-  var person = Person('Gelizon', 18);
-  var person = Person('Paa', 19);
-  var person = Person('Daga', 18);
-  var person = Person('Delis', 18);
+void main() {
+  var student = Student('Gelizon', 18);
+  var ceo = Ceo('Paa', 19);
+  var manager = Manager('Daga', 18);
+  var engineer = Engineer('Delis', 18);
 
-  person1.sayHello(); // Output: Hello! I am Gelizon and I am 18 years old.
-  person2.sayHello(); // Output: Hello! I am Paa and I am 19 years old. 
-  person3.sayHello(); // Output: Hello! I am Daga and I am 18 years old. 
-  person4.sayHello(); // Output: Hello! I am Delis and I am 18 years old. 
+  student.sayHello(); // Output: Hello! I am Gelizon and I am 18 years old.
+  ceo.sayHello(); // Output: Hello! I am Paa and I am 19 years old. 
+  manager.sayHello(); // Output: Hello! I am Daga and I am 18 years old. 
+  engineer.sayHello(); // Output: Hello! I am Delis and I am 18 years old. 
+}
